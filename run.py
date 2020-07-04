3f02381a21c2c810cae9330fc5c399a8eab9228b
Aayush Jain <patni.aayush1998@gmail.com>
2020-07-04 21:57:56 +0530

    Implemented Image Superresolution

--- run.py
+++ run.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+import os
+import numpy as np
+from pathlib import Path
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+from PIL import Image
+from models.ImageSuperResolution import predict as superres_predict
+
+CURRENT_WORKING_DIRECTORY = Path(os.getcwd())
+UPLOAD_FOLDER = CURRENT_WORKING_DIRECTORY/'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'} 
+
+load_dotenv(CURRENT_WORKING_DIRECTORY/'variables.env')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = os.getenv('secretKey')
 
 @app.route("/")
 def home():
@@ -9,6 +24,42 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+def allowed_file(filename):
+    extension = filename.split('.')[1].lower()
+    if extension not in ALLOWED_EXTENSIONS:
+        return False
+    else:
+        return True
+
+@app.route("/superresolution", methods = ['GET', 'POST'])
+def superresolution():
+    if(request.method == 'POST'):
+        if('file' not in request.files):
+            flash('No File')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if(file.filename == ''):
+            flash('No file selected')
+            return redirect(request.url)
+        if(file and allowed_file(file.filename)):
+            filename = secure_filename(file.filename)
+            file.save(UPLOAD_FOLDER/filename)
+            image_file = url_for('static', filename = 'uploads/'+filename)
+            return render_template('superresolution.html', image_file = image_file)
+    return render_template('superresolution.html', image_file = None)
+
+@app.route("/superresolve<image_file>/gans", methods = ['GET', 'POST'])
+def superresolve(image_file):
+    image = Image.open(CURRENT_WORKING_DIRECTORY/'static/uploads'/image_file)
+    predicted_image = superres_predict(image)
+    predicted_image = np.uint8((predicted_image*0.5 + 0.5)*255.0)
+    predicted_image = Image.fromarray(predicted_image)
+    new_name = image_file.split('.')[0] + '_superresolved.jpeg'
+    predicted_image.save(CURRENT_WORKING_DIRECTORY/'static/uploads'/new_name)
+    image_file = url_for('static', filename = 'uploads/' + new_name)
+    return render_template('superresolution.html', image_file = image_file)
 
 if __name__ == '__main__':
     app.run(debug = True)
