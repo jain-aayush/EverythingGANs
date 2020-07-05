8d8b1cb13af5f9654823f03f0ac4dc46df810600
Aayush Jain <patni.aayush1998@gmail.com>
2020-07-05 20:06:49 +0530

    Implemented Image Deraining

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 from PIL import Image
 from models.ImageSuperResolution import predict as superres_predict
+from models.ImageDeraining import predict as derain_predict
 
 CURRENT_WORKING_DIRECTORY = Path(os.getcwd())
 UPLOAD_FOLDER = CURRENT_WORKING_DIRECTORY/'static/uploads'
@@ -60,6 +61,36 @@ def superresolve(image_file):
     predicted_image.save(CURRENT_WORKING_DIRECTORY/'static/uploads'/new_name)
     image_file = url_for('static', filename = 'uploads/' + new_name)
     return render_template('superresolution.html', image_file = image_file)
+
+@app.route("/deraining", methods = ['GET', 'POST'])
+def deraining():
+    if(request.method == 'POST'):
+        if('file' not in request.files):
+            flash('No File')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if(file.filename == ''):
+            flash('No file selected')
+            return redirect(request.url)
+        if(file and allowed_file(file.filename)):
+            filename = secure_filename(file.filename)
+            file.save(UPLOAD_FOLDER/filename)
+            image_file = url_for('static', filename = 'uploads/'+filename)
+            return render_template('derain.html', image_file = image_file)
+    return render_template('derain.html', image_file = None)
+
+@app.route("/derain<image_file>/gans", methods = ['GET', 'POST'])
+def derain(image_file):
+    image = Image.open(CURRENT_WORKING_DIRECTORY/'static/uploads'/image_file)
+    predicted_image = derain_predict(image)
+    predicted_image = np.uint8((predicted_image*0.5 + 0.5)*255.0)
+    predicted_image = Image.fromarray(predicted_image)
+    new_name = image_file.split('.')[0] + '_derained.jpeg'
+    predicted_image.save(CURRENT_WORKING_DIRECTORY/'static/uploads'/new_name)
+    image_file = url_for('static', filename = 'uploads/' + new_name)
+    return render_template('derain.html', image_file = image_file)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
